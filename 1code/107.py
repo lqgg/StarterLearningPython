4fc2068d060dc5bb97aa78443a3081ecc3286f81
lqgg <52057602+lqgg@users.noreply.github.com>
2019-10-22 14:35:28 +0800

    Update 107.py

--- 1code/107.py
+++ 1code/107.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-name = raw_input("What is your name?")
-age = raw_input("How old are you?")
+name = raw_input("What is your name?")#你的名字是什么
 
-print "Your name is:", name
-print "You are " + age + " years old."
+age = raw_input("How old are you?")#你现在多大啦
 
-after_ten = int(age) + 10
-print "You will be " + str(after_ten) + " years old after ten years."
+print "Your name is:", name#打印出你输入的你的名字
+print "You are " + age + " years old."#打印出你输入的你的年龄
+
+after_ten = int(age) + 10#十年之后
+print "You will be " + str(after_ten) + " years old after ten years."#打印出你的十年之后是多少岁
